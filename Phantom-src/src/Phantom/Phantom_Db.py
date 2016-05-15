c2db38e36ce8f3b9a759dd9504d20c3c436f1cee
Shift Team <shiftcurrency@gmail.com>
2016-05-15 15:22:32 +0200

    init database on startup

--- Phantom-src/src/Phantom/Phantom_Db.py
+++ Phantom-src/src/Phantom/Phantom_Db.py
@@ -3,21 +3,13 @@ import Error_Msg
 
 class PhantomDb(object):
 
-    def __init__(self, store_dict)
-        if len(store_dict) == 0:
-            return Error_Msg.error_response("err_store_data")
-
-        try:
-            conn = sqlite3.connect('phantom.db')
-            c = conn.cursor()
-        except Exception as e:
-            return Error_Msg.error_response("err_store_data")
-
+    conn = sqlite3.connect('src/Phantom/phantom.db')
+    c = conn.cursor()
 
     def init_database(self):
         try:
-            c.execute('CREATE TABLE IF NOT EXIST messaging (identity TEXT, filter_id INTEGER)')
-            conn.commit()
+            self.c.execute('CREATE TABLE IF NOT EXISTS messaging (identity TEXT, filter_id INTEGER)')
+            self.conn.commit()
         except Exception as e:
             return Error_Msg.error_response("err_store_data")
 
@@ -26,8 +18,8 @@ class PhantomDb(object):
 
     def clear_database(self):
         try:
-            c.execute('DELETE FROM messaging')
-            conn.commit()
+            self.c.execute('DELETE FROM messaging')
+            self.conn.commit()
         except Exception as e:
             return Error_Msg.error_response("err_store_data")
         
@@ -35,11 +27,11 @@ class PhantomDb(object):
 
 
     def store_data(self, store_dict):
-        if 'to' in store_dict and 'filter_id' in store_dict and \
+        if len(store_dict) > 0 and 'to' in store_dict and 'filter_id' in store_dict and \
             store_dict['filter_id'] != "" and store_dict['to'] != "":
                 try:
-                    c.execute("INSERT OR IGNORE INTO messaging (identity, filter_id) VALUES (?,?)", store_dict['filter_id'], store_dict['to'],)
-                    conn.commit()
+                    self.c.execute("INSERT OR IGNORE INTO messaging (identity, filter_id) VALUES (?,?)", store_dict['filter_id'], store_dict['to'],)
+                    self.conn.commit()
                 except Exception as e:
                     return Error_Msg.error_response("err_store_data")
                 
