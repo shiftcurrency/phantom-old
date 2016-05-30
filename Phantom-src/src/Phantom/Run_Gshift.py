4e0eded368301be7a3cdff36260066d826e3a465
Shift Team <shiftcurrency@gmail.com>
2016-05-30 09:57:51 +0200

    windows needs (as usual) some special tweaking..

--- Phantom-src/src/Phantom/Run_Gshift.py
+++ Phantom-src/src/Phantom/Run_Gshift.py
@@ -1,8 +1,14 @@
 from subprocess import Popen, PIPE
+from sys import platform
 
 def start():
     try:
-        process = Popen(['gshift', '--shh'], stdout=PIPE, stderr=PIPE, shell=False)
+        if platform == 'win32':
+            command = ['gshift.exe', '--shh', '--rpccorsdomain="null"']
+        else:
+            command = ['gshift', '--shh']
+
+        process = Popen(command, stdout=PIPE, stderr=PIPE, shell=False)
         print "- Started gshift."
         return process
     except Exception as e:
