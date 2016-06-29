27bba92a456dcad62b1942f076d8ba9fbad5f7e3
Shift Team <shiftcurrency@gmail.com>
2016-06-29 17:34:04 +0200

    testing redirect to null. problem with gshift child process that does to sleep when buffer is full.

--- Phantom-src/src/Phantom/Run_Gshift.py
+++ Phantom-src/src/Phantom/Run_Gshift.py
@@ -1,4 +1,5 @@
-from subprocess import Popen, PIPE
+from os import devnull
+from subprocess import Popen
 from sys import platform
 from Debug import Debug
 import logging
@@ -9,16 +10,17 @@ class Run_Gshift:
     def __init__(self):
         self.log = logging.getLogger("gshift")
         self.process = None
+        self.fnull = open(devnull, 'wb')
 
     def start(self):
         self.log.info("Starting gshift...")
         try:
             if platform == 'win':
                 command = ['gshift.exe', '--shh']
-                self.process = Popen(command, stdout=PIPE, stderr=PIPE, shell=False)
+                self.process = Popen(command, stdout=self.fnull, stderr=self.fnull, shell=False, close_fds=True)
             else:
                 command = ['gshift', '--shh']
-                self.process = Popen(command, stdout=PIPE, stderr=PIPE, shell=False)
+                self.process = Popen(command, stdout=self.fnull, stderr=self.fnull, shell=False, close_fds=True)
 
                 # Terminate on exit
                 atexit.register(self.stop)
