802c3101e745d2e5f5fe44d95a0fd802d6651033
Shift Team <shiftcurrency@gmail.com>
2016-07-29 16:40:29 +0200

    better handleing for platform identification

--- Phantom-src/src/Contract/tools/utils.py
+++ Phantom-src/src/Contract/tools/utils.py
@@ -3,9 +3,10 @@ try:
     from Crypto.Hash import keccak
     sha3_256 = lambda x: keccak.new(digest_bits=256, data=x).digest()
 except:
-    if sys.platform == 'linux':
+    print sys.platform
+    if 'linux' in sys.platform:
         from sha3_linux64 import sha3 as _sha3
-    elif sys.platform == 'win32':
+    elif 'win' in sys.platform:
         from sha3_win32 import sha3 as _sha3
     sha3_256 = lambda x: _sha3.sha3_256(x).digest()
 from lib.pybitcointools import bitcoin
