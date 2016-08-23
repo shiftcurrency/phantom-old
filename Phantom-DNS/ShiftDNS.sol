8467c738bd8ad32d4797085d496a645907c788b4
Shift Team <shiftcurrency@gmail.com>
2016-08-23 15:43:24 +0200

    new draft of DNS contract

--- Phantom-DNS/ShiftDNS.sol
+++ Phantom-DNS/ShiftDNS.sol
@@ -7,13 +7,25 @@ contract ShiftDNS{
     }
     
     mapping( string => RR ) rrs;
+
+    uint256[] domains;
+
+    function addDomainList(uint256 domain) {
+        domains.push(domain);
+    }
+
+    function getDomainList() returns (uint256[]) {
+        return domains;
+    }
     
-    function setRR(string _domain, string _address){
+    function setRR(string _domain, string _address) returns (bool){
         if(rrs[_domain].exist == false) {
             rrs[_domain].addr = _address;
             rrs[_domain].owner = msg.sender;
             rrs[_domain].exist = true;
+            return true;
         }
+        return false;
     }
 
     function getRR(string _domain) returns (string) {
