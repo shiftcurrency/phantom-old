bacac459e16420bf1b15f6c1104dd1f7a55ba1f7
Shift Team <shiftcurrency@gmail.com>
2016-08-31 23:04:10 +0200

    added function for setting balance limit for indexing sites. All the core team member has access to set it.

--- Phantom-DNS/ShiftDNS.sol
+++ Phantom-DNS/ShiftDNS.sol
@@ -17,39 +17,36 @@ contract ShiftDNS {
     mapping( string => RR ) rrs;
     mapping( string => SiteIndex ) index;
     address address1 = 0x82b5f6de85e6d5982992ee467808682b01a95e24;
-    uint accountBalance;
+    address address2 = 0x1050e68083a5378a02deec6a23368a788d23fd6c;
+    address address3 = 0x1c581f7803508f4bc01e9b7595b220be2a5f1818;
+    address address4 = 0xcad21b2e2c06d79d86411d1577714937c9c7aad9; 
+    uint accountBalanceLimit;
 
-    function siteIndexBalance(uint balance) returns (uint){
-        if (msg.sender == address1) {
-            accountBalance = balance;
+    function setIndexingBalanceLimit(uint balance) returns (uint){
+        if (msg.sender == address1 || msg.sender == address2 || msg.sender == address3 || msg.sender == address4) {
+            accountBalanceLimit = balance;
             // Returns 0 if the update of balance check is successfully set.
             return 0;
         }
         // Returns 0 if the update of balance check fail.
         return 1;
     }
-            
 
-    function setRR(string _domain, string _address) returns (bool){
-        // Check if the domain exists, else register it. 
-        if(rrs[_domain].exist == false) {
-            rrs[_domain].addr = _address;
-            rrs[_domain].owner = msg.sender;
-            rrs[_domain].exist = true;
-            return true;
-        }
-        return false;
+    function getBalanceLimit() returns (uint) {
+        return accountBalanceLimit;
     }
 
     function indexSite(string owner, string _domain, string _phantom_addr) returns (uint) {
+
+        // OBSERVE: You need a registered domain to be able to your site to the global mesh index.
 
         // The account that calls the contract.
         address sndr = msg.sender;
         // The balance of the account that calls the contract.
         uint balance = sndr.balance;
 
-        // Checks the account balance limit before creating a domain.
-        if(balance >= accountBalance) {
+        // Checks the account balance limit before indexing a site.
+        if(balance >= accountBalanceLimit) {
             if(index[owner].exist == false) {
                 index[owner].exist = true;
                 index[owner].domain = _domain;
@@ -65,13 +62,22 @@ contract ShiftDNS {
     }
 
     function searchIndex(string addr) returns (string) {
-        // Balance check is already done since phantom use the function indexSite to resolve domain names.
-        // You can index a site, but it wont get indexed if a registered domain does not exist.
         if(index[addr].exist == true) {
             return index[addr].domain;
         }
         return "false";
     }
+
+    function setRR(string _domain, string _address) returns (uint){
+        // Check if the domain exists, else register it. 
+        if(rrs[_domain].exist == false) {
+            rrs[_domain].addr = _address;
+            rrs[_domain].owner = msg.sender;
+            rrs[_domain].exist = true;
+            return 0;
+        }   
+        return 1;
+    }   
 
     // Return the Phantom bitcoin hash address for the site.
     function getRR(string _domain) returns (string) {
